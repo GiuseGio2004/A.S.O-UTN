5fc0a07619a54285dff4cd082ca11a7d07efa3da
GiuseGio2004 <166439113+GiuseGio2004@users.noreply.github.com>
2024-06-24 11:14:47 -0300

    Update ObtenerClima.sh

--- TP 5/Parte 2/ObtenerClima.sh
+++ TP 5/Parte 2/ObtenerClima.sh
@@ -8,4 +8,16 @@ url="http://api.weatherapi.com/v1/current.json?key=$api_key&q=$ciudad"
 
 echo "Obteniendo datos del clima..."
 
-curl -s $url | jq '.current | {temp_c: .temp_c, condicion: .condition.text, viento_kph: .wind_kph, humedad: .humidity}'
+temp_actual=$(curl -s $url | jq '.current.temp_c')
+
+echo "La temperatura actual en $ciudad es de $temp_actual°C."
+
+# Pregunta al usuario si desea cambiar la temperatura
+read -p "¿Deseas cambiar la temperatura? (s/n): " cambiar_temp
+
+if [ "$cambiar_temp" == "s" ]; then
+  read -p "Introduce la nueva temperatura: " nueva_temp
+  echo "La temperatura en $ciudad ha sido ajustada a $nueva_temp°C."
+else
+  echo "La temperatura en $ciudad se mantiene en $temp_actual°C."
+fi
